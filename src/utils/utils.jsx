d2d7c5846364a28315fb003d5c24138497199730
Kris Kurzawa <kurzawa1@yahoo.com>
2024-02-27 13:54:31 -0500

    initial commit

---
+++ src/utils/utils.jsx
@@ -0,0 +1,20 @@
+import BlueEyesForPalestine from '../assets/audio/BlueEyesForPalestine.mp3'
+import ElaGosta from '../assets/audio/ElaGosta.mp3';
+import ZapToro from '../assets/images/ZapToro.jpeg';
+import VisionQuest from '../assets/images/VisionQuest.jpg';
+
+export const tracks = [
+    {
+        title: 'Blue Eyes for Palestine',
+        src: BlueEyesForPalestine,
+        author: 'Zap Toro',
+        thumbnail: ZapToro,
+    },
+    {
+        title: 'Ela Gosta',
+        src: ElaGosta,
+        author: 'Zap Toro',
+        thumbnail: VisionQuest,
+    },
+
+];
